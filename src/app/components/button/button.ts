bf456990c93792127e7f8ba9f2b2155ff6cfda7b
NedimMajdanac <nedimmajdanac13@gmail.com>
2025-12-19 00:50:28 +0100

    Added header and button components

---
+++ src/app/components/button/button.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  imports: [CommonModule],
+  templateUrl: './button.html',
+  styleUrl: './button.css',
+})
+export class Button implements OnInit{
+
+  @Input() text!: string;
+  @Input() color!: string;
+
+
+  constructor() { }
+  ngOnInit(): void{}
+}
---
+++ src/app/components/header/header.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Button } from '../button/button';
+
+@Component({
+  selector: 'app-header',
+  imports: [Button],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header implements OnInit {
+  title: string  = "task-tracker"
+  constructor() {}
+  ngOnInit(): void{}
+}
